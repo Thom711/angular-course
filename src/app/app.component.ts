9639fa081e710f254840b0df607497bcf102af3c
Thom711 <tvdveldt@hotmail.com>
2021-03-16 09:52:02 +0100

    init, lesson 1-2-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course';
+  data = {
+    title: 'Hello World!',
+    author: 'Thom van der Veldt',
+  };
 }
